000fe2d6f77e1430d56df384863bacb43900d550
Priscilla Chan <30789820+priichan@users.noreply.github.com>
2018-11-12 16:25:18 -0800

    testing first commit

--- map_tester.cc
+++ map_tester.cc
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]) {
 	LLRB_map<int, std::string> llrb1;
-	std::vector<std::string> str_vector = { "Priscilla", "and", "Laura", "are", "awesome" };
+	std::vector<std::string> str_vector = { "Priscilla", "and", "Laura", "are", "awesome af" };
 	int i = 0;
 	for (std::string str_i : str_vector) {
 		llrb1.Insert(i, str_i);
