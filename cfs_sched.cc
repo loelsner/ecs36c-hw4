48bf9f320c8148ba6829b7baf034523159ed973b
Laura Oelsner <lroelsner@ucdavis.edu>
2018-11-14 18:05:36 -0800

    working input, tree insert

--- cfs_sched.cc
+++ cfs_sched.cc
@@ -2,10 +2,27 @@
 
 #include "llrb_multimap.h"
 
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+	std::ifstream in(argv[1]);
+
+	if (in.good()) std::cout << "its good";
+
 	LLRB_multimap<char, int> CFS;
+	while (in.peek() != EOF) {
+		char key;
+		in >> key;
+		int startTime;
+		in >> startTime;
+		int duration;
+		in >> duration;
+		CFS.Insert(key, startTime);
+		CFS.Insert(key, duration);
+	}
+
+	CFS.Print();
 
 	std::cout << " Hi";
 	return 0;
